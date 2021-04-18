076cf9fdeb20100bb4444764ea67937b701d7812
Samuel Solomon <mrsamuelsolomon@gmail.com>
2021-04-18 19:07:59 +0100

    homepage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import './App.scss' ;
 import NavBar from './components/NavBar' ;
+import Homepage from './pages/Homepage';
+import { Switch, Route } from 'react-router-dom' ;
+import AboutPage from './pages/AboutPage';
 
 
 function App() {
@@ -8,7 +11,18 @@ function App() {
       <div className="sidebar">
         <NavBar />
       </div>
-      <div className="main-content"></div>
+      <div className="main-content">
+        <div className="content">
+          <Switch>
+            <Route path="/" exact>
+              <Homepage />
+            </Route>
+            <Route path="/about" exact>
+              <AboutPage />
+            </Route>
+          </Switch>
+        </div>
+      </div>
     </div>
   );
 }
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from 'react' ;
+import {Link} from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faFacebook} from '@fortawesome/free-brands-svg-icons';
+import {faGithub} from '@fortawesome/free-brands-svg-icons';
+import {faYoutube} from '@fortawesome/free-brands-svg-icons';
+
 
 function Homepage() {
     return (
-        <div>
-            <h1>Homepage</h1>
+        <div className="HomePage">
+            <header className="hero">
+                <h1 className="hero-text">
+                    Hi, I am
+                    <span> Lorem Ipsum</span>
+                </h1>
+                <p className="h-sub-text">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing 
+                    elit. Soluta veniam voluptate, numquam at est fugit 
+                    aperiam sint omnis dignissimos voluptatum et ducimus 
+                    ratione recusandae commodi distinctio dolorem! Ipsa, 
+                    molestiae laborum. 
+                </p>
+                <div className="icons">
+                    <Link to="test" className="icon-holder">
+                        <FontAwesomeIcon icon={faFacebook} className="icon fb" />
+                    </Link>
+                    <Link to="test" className="icon-holder">
+                        <FontAwesomeIcon icon={faGithub} className="icon gh" />
+                    </Link>
+                    <Link to="test" className="icon-holder">
+                        <FontAwesomeIcon icon={faYoutube} className="icon yt"/>
+                    </Link>
+                </div>
+            </header>
         </div>
     )
 }
