fb62120cbfbfdbef2c34b7a2e4c45135ee108ae1
Samuel Solomon <mrsamuelsolomon@gmail.com>
2021-04-20 07:02:11 +0100

    aboutpage

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,12 +1,42 @@
-import React from 'react'
-import ImageSection from '../components/ImageSection'
-import Title from '../components/Title'
+import React from 'react' ;
+import ImageSection from '../components/ImageSection' ;
+import ServicesSection from '../components/ServicesSection';
+import SkillsSection from '../components/SkillsSection';
+import Title from '../components/Title' ;
+import design from '../img/design.svg' ;
+import intelligence from '../img/intelligence.svg';
+import gamedev from '../img/game-dev.svg';
 
 function AboutPage() {
     return (
         <div className="AboutPage">
             <Title title={'About Me'} span={'About Me'}/>
             <ImageSection />
+            <Title title={'My Skills'} span={'My Skills'}/>
+            <div className="skillsContainer">
+                <SkillsSection skill={'Javascript'} progress={'70%'} width={'70%'} />
+                <SkillsSection skill={'Typescript'} progress={'30%'} width={'30%'} />
+                <SkillsSection skill={'React Js'} progress={'70%'} width={'70%'} />
+                <SkillsSection skill={'Node Js'} progress={'80%'} width={'80%'} />
+                <SkillsSection skill={'Python'} progress={'90%'} width={'90%'} />
+                <SkillsSection skill={'Lua'} progress={'40%'} width={'40%'} />
+                <SkillsSection skill={'Java'} progress={'80%'} width={'80%'} />
+                <SkillsSection skill={'Web Design'} progress={'25%'} width={'25%'} />
+                <SkillsSection skill={'UI/Ux Design'} progress={'76%'} width={'76%'} />
+            </div>
+
+            <Title title={'Services'} span={'Services'}/>
+            <div className="servicesContainer">
+                <ServicesSection image={design} title={'Web design'} 
+                text={'Lorem ipsum dolor sit amet consectetur adipisicing elit.'}
+                />
+                <ServicesSection image={intelligence} title={'Artificial Intelligence'} 
+                text={'Lorem ipsum dolor sit amet consectetur adipisicing elit.'}
+                />
+                <ServicesSection image={gamedev} title={'Game Development'} 
+                text={'Lorem ipsum dolor sit amet consectetur adipisicing elit.'}
+                />
+            </div>
         </div>
     )
 }
