2b8a5e7062a00a00ad5b0d95826d98331afe1b43
Samuel Solomon <mrsamuelsolomon@gmail.com>
2021-04-20 07:52:11 +0100

    blogspage

--- src/pages/BlogsPage.js
+++ src/pages/BlogsPage.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react' ;
+import Title from '../components/Title' ;
+import allBlogs from '../components/allBlogs' ;
 
 function BlogsPage() {
     return (
         <div>
-            <h1>Blogs Page</h1>
+            <div className="b-title">
+                <Title title={'Recent Blogs'} span={'Recent Blogs'}/>
+            </div>
+            <div className="BlogsPage">
+                {
+                    allBlogs.map((blog) => {
+                        return <div className="blog" key={blog.id}>
+                            <div className="blog-content">
+                                <img src={blog.image} alt=""/>
+                                <a href={blog.link} className="blog-link">
+                                {blog.title}
+                                </a>
+                            </div>
+                        </div>
+                    })
+                }
+            </div>
         </div>
     )
 }
