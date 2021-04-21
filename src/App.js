abeb989e5cb89a561153c02279edaa3b7b9856d5
Samuel Solomon <mrsamuelsolomon@gmail.com>
2021-04-21 08:16:10 +0100

    homepage media queries

--- src/App.js
+++ src/App.js
@@ -6,13 +6,31 @@ import AboutPage from './pages/AboutPage';
 import PortfolioPage from './pages/PortfolioPage' ;
 import BlogsPage from './pages/BlogsPage' ;
 import ContactPage from './pages/ContactPage' ;
+import { useState } from 'react';
+
 
 
 function App() {
+  const [navToggle, setNavToggle] = useState(false);
+
+  const navClick = () =>{
+    setNavToggle(!navToggle)
+  }
+
+
+
+
+
+
   return (
     <div className="App">
-      <div className="sidebar">
+      <div className={`sidebar ${navToggle ? 'nav-toggle': ''}`}>
         <NavBar />
+      </div>
+      <div className="nav-btn" onClick={navClick}>
+        <div className="lines-1"></div>
+        <div className="lines-2"></div>
+        <div className="lines-3"></div>
       </div>
       <div className="main-content">
         <div className="content">
